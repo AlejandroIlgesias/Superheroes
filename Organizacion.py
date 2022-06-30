af419f48d0bb559bf0114649ae2f9207974cce30
AlejandroIlgesias <93452718+AlejandroIlgesias@users.noreply.github.com>
2022-06-30 12:55:03 +0200

    Update Organizacion.py

--- Organizacion.py
+++ Organizacion.py
@@ -1,3 +1,5 @@
 class Organizacion():
   def __init__(self,nombre,miembros,escenario):
+     organizaciones=["A-Force", "Avengers", "Mercs for Money","League of Realms", "Strange Academy" ,"X-Men"]
+        
 
