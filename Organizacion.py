25263ba26a2b89b7a2c55154507da7c6716e8f56
AlejandroIlgesias <93452718+AlejandroIlgesias@users.noreply.github.com>
2022-06-30 12:55:27 +0200

    Update Organizacion.py

--- Organizacion.py
+++ Organizacion.py
@@ -1,5 +1,7 @@
 class Organizacion():
   def __init__(self,nombre,miembros,escenario):
      organizaciones=["A-Force", "Avengers", "Mercs for Money","League of Realms", "Strange Academy" ,"X-Men"]
+      if type(nombre)==str:
+            if nombre in organizaciones:
         
 
